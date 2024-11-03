7e76d5e2bba8a551882e3efbe35fa2e89eb85442
lukadizdar <llllukadizdar@gmail.com>
2024-11-03 22:09:52 +0100

    Second initial commit

--- src/main.c
+++ src/main.c
@@ -9,6 +9,14 @@ int main(void) {
     int sum = compute_sum(numbers, numberCount);
     float avg = compute_average(numbers, numberCount);
     float movavg = compute_moving_average(numbers, numberCount, 3);
+    FILE *file1 = fopen("output.txt", "r");
+    fseek (file1, 0, SEEK_END);
+    long size = ftell(file1);
+    fclose(file1);
+    if (size != 0) {
+        FILE *file2 = fopen("output.txt", "w");
+        fclose(file2);
+    }
     write_result("output.txt", (float)sum);
     write_result("output.txt", avg);
     write_result("output.txt", (movavg));
